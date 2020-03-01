d139b2ebfd48446e80aa6e2304b690a0251a3249
by-bai <58441020+by-bai@users.noreply.github.com>
2020-03-01 21:49:44 +0800

    added reply-keyboard and conversationhandler for telebot
    replaced inline keyboard with reply keyboard, conversation handler to set flow of convo

--- telegram-bot/telebot.py
+++ telegram-bot/telebot.py
@@ -1,51 +1,103 @@
 import logging # to inform when and why things don't work as expected
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram import ReplyKeyboardMarkup 
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-my_token = 'insert token here'
+my_token = '1021081111:AAEe7HbsD4tRE9RrndctArLggjH1Z-6IPiU'
 
-def start(update, context):
+# Flow of Conversation
+MATRIC_VALIDITY, FILTER_LOCATION, FILTER_SIZE, AVAIL_LOCKER, BOOKING_CONFIRMED = range(5) 
+
+# Store user info in temp dictionary for referencing 
+user_info = {} 
+
+
+# Start - Introduction to Lock-It-Down Bot & User inputs Matriculation Number 
+def start(update, context): 
+
     context.bot.send_message(chat_id=update.effective_chat.id, 
-                             text="Hello Student! \n \nHeard you need a locker! Our locker rates are at $0.03/hour (+ more details), please select /getlocker to start your booking!")
+                            text='Hello Student! \n \n'
+                            'Heard you need a locker! Our locker rates are at $0.03/hour. '
+                            'You can request a locker by your desired location and locker size. \n \n'
+                            'Before we begin, please enter your SMU Matriculation Number: ')
 
-def get_locker(update, context):
-    keyboard = [[InlineKeyboardButton("Location", callback_data='Location'),
-                 InlineKeyboardButton("Locker Type", callback_data='LockerType')],
+    return MATRIC_VALIDITY
 
-                [InlineKeyboardButton("Cancel", callback_data='Cancel')]]
 
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Filter your locker request by...', reply_markup=reply_markup)
+# Check for validity of Matriculation Number (8 characters)
+def matric_validity(update, context):
 
+    matric_id = update.message.text
+    user_info['matric id'] = matric_id
+
+    if len(str(matric_id)) == 8:
+        reply_keyboard = [['YES']]
+        update.message.reply_text("Alright! Select 'YES' to start your booking. \n \n"
+                                  "Note: You may type /cancel at any time to terminate your session.", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+        return FILTER_LOCATION 
+
+    # else:
+    #     print('exception case needed') # (!) Need to code exception case      
+
+
+# Select Filter Location 
 def filter_location(update, context):
-    keyboard = [[InlineKeyboardButton("LKCSB", callback_data='LKCSB'),
-                 InlineKeyboardButton("SOA", callback_data='SOA'),
-                 InlineKeyboardButton("SOE/SOSS", callback_data='SOE/SOSS'),
-                 InlineKeyboardButton("SOL", callback_data='SOL'),
-                 InlineKeyboardButton("SIS", callback_data='SIS')],
 
-                [InlineKeyboardButton("Cancel", callback_data='Cancel')]]
+    reply_keyboard = [['LKCSB', 'SOA', 'SOE'], ['SOL', 'SIS']]
+    update.message.reply_text('Select Locker Location:', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Select Locker Location:', reply_markup=reply_markup)
+    return FILTER_SIZE 
 
 
-def button(update, context):
-    query = update.callback_query # user's response
-    query.edit_message_text(text="Selected Option: {}".format(query.data))
+# Select Filter Locker Size 
+def filter_size(update, context):
 
-    if query.data == 'Location':
-        filter_location(update, context)  
+    location = update.message.text
+    user_info['location'] = location
+
+    reply_keyboard = [['S'], ['M'], ['L']]
+    update.message.reply_text('Select Locker Size:', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return AVAIL_LOCKER 
 
 
-def help(update, context):
-    update.message.reply_text("Use /start to test this bot.")
+# Select From Available Lockers
+## temporary dummy values, need GET request to pull available lockers from database, based on user's filter requests. ##
+def avail_locker(update, context):
+
+    size = update.message.text
+    user_info['locker size'] = size
+
+    reply_keyboard = [['SIS-L1-01'], ['SIS-L1-05'], ['SIS-L2-11'], ['SIS-L3-01'], ['SIS-L3-12'], ['SIS-L3-33']] 
+    update.message.reply_text('Select Locker To Confirm Booking:', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return BOOKING_CONFIRMED
+
+
+def booking_confirmed(update, context): 
+
+    selected = update.message.text
+    user_info['selected locker'] = selected
+
+    update.message.reply_text('Done! Please head to {}'.format(user_info['selected locker'])) 
+
+    print(user_info)
+
+    return ConversationHandler.END 
+
+
+def cancel(update, context): 
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text("Your session has been terminated. Please type /start to begin a new one.")
+
+    return ConversationHandler.END
 
 
 def error(update, context):
@@ -56,12 +108,31 @@ def error(update, context):
 def main():
     updater = Updater(my_token, use_context=True)
 
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-    updater.dispatcher.add_handler(CommandHandler('help', help))
-    updater.dispatcher.add_handler(CommandHandler('getlocker', get_locker))
-    updater.dispatcher.add_handler(CommandHandler('filterlocation', filter_location))
-    updater.dispatcher.add_error_handler(error)
+    dp = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+
+            MATRIC_VALIDITY: [MessageHandler(Filters.text, matric_validity)], # must receive text(matric id) first
+
+            FILTER_LOCATION: [MessageHandler(Filters.regex('^(YES)$'), filter_location)], # must receive YES 
+
+            FILTER_SIZE: [MessageHandler(Filters.regex('^(LKCSB|SOA|SOE|SOL|SIS)$'), filter_size)], # must receive locker location first
+
+            AVAIL_LOCKER: [MessageHandler(Filters.regex('^(S|M|L)$'), avail_locker)], # must receive locker size first
+
+            BOOKING_CONFIRMED: [MessageHandler(Filters.text, booking_confirmed)] # (!!!) Need to change filter to accept locker xxx-xx-xx only 
+
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dp.add_handler(conv_handler)
+    dp.add_error_handler(error)
+
 
     # Start the Bot
     updater.start_polling()
