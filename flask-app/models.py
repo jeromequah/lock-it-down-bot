fa6b1d90d044a692b2e80ac16cf6996d9af4d552
Rome.Q <jerome.quah.2018@sis.smu.edu.sg>
2020-03-26 14:35:46 +0800

    Updated.py
    timeout = db.Column()

--- flask-app/models.py
+++ flask-app/models.py
@@ -23,8 +23,8 @@ class Booking(db.Model):
 	bookingID = db.Column(db.Integer, primary_key=True)
 	matric = db.Column(db.String(8), db.ForeignKey('student.matric'), nullable=False)
 	lockerName = db.Column(db.String(80), db.ForeignKey('locker.lockerName'), nullable=False)
-	timein = db.Column(db.DateTime,default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
-	timeout = db.Column(db.DateTime,default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+	timein = db.Column(db.DateTime,default=datetime.datetime.utcnow)
+	timeout =  db.Column()
 
 #one to many
 	student_info = db.relationship('Student', back_populates='student_booking')
